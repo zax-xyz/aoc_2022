61a6ae3b46985dc9d636ba85f2b69d249290875b
Michael Vo <zax@zaxu.xyz>
2022-12-01 23:07:49 +1100

    feat(day01): add part 2

--- src/day01.rs
+++ src/day01.rs
@@ -10,3 +10,15 @@ pub fn part1() -> u32 {
         .max()
         .unwrap()
 }
+
+pub fn part2() -> u32 {
+    let mut calories = fs::read_to_string("inputs/day01p1.txt")
+        .expect("Reading file failed :(")
+        .lines()
+        .collect::<Vec<&str>>()
+        .split(|line| line.len() == 0)
+        .map(|group| group.iter().map(|l| str::parse::<u32>(l).unwrap()).sum())
+        .collect::<std::collections::BinaryHeap<u32>>();
+
+    (0..3).map(|_| calories.pop().unwrap()).sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod day01;
 
 fn main() {
     println!("{}", day01::part1());
+    println!("{}", day01::part2());
 }
