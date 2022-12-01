e017a4f1445a7b44622b5b21e8b075129b6dc5b3
Michael Vo <zax@zaxu.xyz>
2022-12-02 00:29:52 +1100

    refactor(day01): use itertools group_by
    stop collecting everything into vecs for performance

--- src/day01.rs
+++ src/day01.rs
@@ -1,12 +1,17 @@
+extern crate itertools;
+
+use itertools::Itertools;
+use std::collections::BinaryHeap;
 use std::fs;
 
 pub fn part1() -> u32 {
     fs::read_to_string("inputs/day01p1.txt")
         .expect("Reading file failed :(")
         .lines()
-        .collect::<Vec<&str>>()
-        .split(|line| line.len() == 0)
-        .map(|group| group.iter().map(|l| str::parse::<u32>(l).unwrap()).sum())
+        .group_by(|line| line.len() == 0)
+        .into_iter()
+        // assumes parsing only fails on the blank lines
+        .filter_map(|(_, group)| group.map(|l| str::parse::<u32>(l).ok()).sum())
         .max()
         .unwrap()
 }
@@ -15,10 +20,15 @@ pub fn part2() -> u32 {
     let mut calories = fs::read_to_string("inputs/day01p1.txt")
         .expect("Reading file failed :(")
         .lines()
-        .collect::<Vec<&str>>()
-        .split(|line| line.len() == 0)
-        .map(|group| group.iter().map(|l| str::parse::<u32>(l).unwrap()).sum())
-        .collect::<std::collections::BinaryHeap<u32>>();
+        .group_by(|line| line.len() == 0)
+        .into_iter()
+        // explicitly filters out blank lines before trying to parse
+        .filter_map(|(_, group)| {
+            group
+                .map(|l| (l.len() > 0).then(|| str::parse::<u32>(l).unwrap()))
+                .sum()
+        })
+        .collect::<BinaryHeap<u32>>();
 
     (0..3).map(|_| calories.pop().unwrap()).sum()
 }
