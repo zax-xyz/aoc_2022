df0057ebf44be9ea202aa67ad59c8640d4640912
Michael Vo <zax@zaxu.xyz>
2022-12-02 23:56:22 +1100

    refactor(day01): use read_lines
    don't collect anything into a vec

--- src/day01.rs
+++ src/day01.rs
@@ -2,30 +2,28 @@ extern crate itertools;
 
 use itertools::Itertools;
 use std::collections::BinaryHeap;
-use std::fs;
+use crate::util::read_lines;
 
 pub fn part1() -> u32 {
-    fs::read_to_string("inputs/day01p1.txt")
+    read_lines("inputs/day01p1.txt")
         .expect("Reading file failed :(")
-        .lines()
-        .group_by(|line| line.len() == 0)
+        .group_by(|line| line.as_ref().unwrap().len() == 0)
         .into_iter()
         // assumes parsing only fails on the blank lines
-        .filter_map(|(_, group)| group.map(|l| str::parse::<u32>(l).ok()).sum())
+        .filter_map(|(_, group)| group.map(|l| str::parse::<u32>(&l.unwrap()).ok()).sum())
         .max()
         .unwrap()
 }
 
 pub fn part2() -> u32 {
-    let mut calories = fs::read_to_string("inputs/day01p1.txt")
+    let mut calories = read_lines("inputs/day01p1.txt")
         .expect("Reading file failed :(")
-        .lines()
-        .group_by(|line| line.len() == 0)
+        .group_by(|line| line.as_ref().unwrap().len() == 0)
         .into_iter()
         // explicitly filters out blank lines before trying to parse
         .filter_map(|(_, group)| {
             group
-                .map(|l| (l.len() > 0).then(|| str::parse::<u32>(l).unwrap()))
+                .map(|l| (l.as_ref().unwrap().len() > 0).then(|| str::parse::<u32>(&l.unwrap()).unwrap()))
                 .sum()
         })
         .collect::<BinaryHeap<u32>>();
