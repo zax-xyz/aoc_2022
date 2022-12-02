0357ecc86a2f00d7aabaa31724b93e8a65b55560
Michael Vo <zax@zaxu.xyz>
2022-12-03 00:14:22 +1100

    refactor(day01/part2): use more accurate variable name

--- src/day01.rs
+++ src/day01.rs
@@ -1,8 +1,8 @@
 extern crate itertools;
 
+use crate::util::read_lines;
 use itertools::Itertools;
 use std::collections::BinaryHeap;
-use crate::util::read_lines;
 
 pub fn part1() -> u32 {
     read_lines("inputs/day01p1.txt")
@@ -18,17 +18,18 @@ pub fn part1() -> u32 {
 pub fn part2() -> u32 {
     let mut calories_heap = BinaryHeap::<i32>::new();
 
-    let lines = read_lines("inputs/day01p1.txt")
+    let groups = read_lines("inputs/day01p1.txt")
         .expect("Reading file failed :(")
         .group_by(|line| line.as_ref().unwrap().len() == 0);
 
-    let mut calories =
-        lines
+    let mut calories = groups
         .into_iter()
         // explicitly filters out blank lines before trying to parse
         .filter_map(|(_, group)| {
             group
-                .map(|l| (l.as_ref().unwrap().len() > 0).then(|| str::parse::<i32>(&l.unwrap()).unwrap()))
+                .map(|l| {
+                    (l.as_ref().unwrap().len() > 0).then(|| str::parse::<i32>(&l.unwrap()).unwrap())
+                })
                 .sum::<Option<i32>>()
         });
 
