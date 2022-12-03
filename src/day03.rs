db55fdcb71189533492c7d2d82a9960131855eab
Michael Vo <zax@zaxu.xyz>
2022-12-04 03:47:21 +1100

    feat(day03): part 2

--- src/day03.rs
+++ src/day03.rs
@@ -1,10 +1,17 @@
 use crate::util::read_lines;
+use itertools::Itertools;
 use std::collections::HashSet;
 
 fn priorities(line: &str) -> HashSet<u32> {
-    line
-        .bytes()
-        .map(|b| (b as i32 + if b.is_ascii_uppercase() { 27 - b'A' as i32 } else { 1 - b'a' as i32 }) as u32)
+    line.bytes()
+        .map(|b| {
+            (b as i32
+                + if b.is_ascii_uppercase() {
+                    27 - b'A' as i32
+                } else {
+                    1 - b'a' as i32
+                }) as u32
+        })
         .collect()
 }
 
@@ -22,5 +29,20 @@ pub fn part1() -> u32 {
 }
 
 pub fn part2() -> u32 {
-    0
+    read_lines("inputs/day03.txt")
+        .unwrap()
+        .chunks(3)
+        .into_iter()
+        .map(|mut lines| -> u32 {
+            let initial = priorities(&lines.next().unwrap().ok().unwrap());
+            lines
+                .fold(initial, |s, line| {
+                    s.intersection(&priorities(&line.unwrap()))
+                        .copied()
+                        .collect()
+                })
+                .iter()
+                .sum()
+        })
+        .sum()
 }
