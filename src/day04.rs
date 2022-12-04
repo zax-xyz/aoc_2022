17e37c60ef50c15638885f98f1823ba293f0a3c0
Michael Vo <zax@zaxu.xyz>
2022-12-04 16:54:50 +1100

    refactor(day04): use usize

--- src/day04.rs
+++ src/day04.rs
@@ -22,18 +22,14 @@ fn contains(interval1: &Interval, interval2: &Interval) -> bool {
     interval1.0 <= interval2.0 && interval1.1 >= interval2.1
 }
 
-pub fn part1() -> u32 {
+pub fn part1() -> usize {
     pairs()
         .filter(|(first, second)| contains(first, second) || contains(second, first))
         .count()
-        .try_into()
-        .unwrap()
 }
 
-pub fn part2() -> u32 {
+pub fn part2() -> usize {
     pairs()
         .filter(|(first, second)| first.0 <= second.1 && first.1 >= second.0)
         .count()
-        .try_into()
-        .unwrap()
 }
