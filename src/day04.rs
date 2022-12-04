60e4fcfad8440b1562492de5fb9a95d8dbbca479
Michael Vo <zax@zaxu.xyz>
2022-12-04 16:51:01 +1100

    refactor(day04): extract pairs function

--- src/day04.rs
+++ src/day04.rs
@@ -1,47 +1,38 @@
 use crate::util::read_lines;
 use itertools::Itertools;
 
-fn contains(interval1: (u32, u32), interval2: (u32, u32)) -> bool {
+type Interval = (u32, u32);
+type Pair = (Interval, Interval);
+
+fn pairs() -> impl Iterator<Item = Pair> {
+    read_lines("inputs/day04.txt").map(|line| -> Pair {
+        line.split(',')
+            .map(|p| -> (u32, u32) {
+                p.split('-')
+                    .map(|n| str::parse::<u32>(n).ok().unwrap())
+                    .next_tuple()
+                    .unwrap()
+            })
+            .next_tuple()
+            .unwrap()
+    })
+}
+
+fn contains(interval1: &Interval, interval2: &Interval) -> bool {
     interval1.0 <= interval2.0 && interval1.1 >= interval2.1
 }
 
 pub fn part1() -> u32 {
-    read_lines("inputs/day04.txt")
-        .filter(|line| {
-            let (first, second) = line
-                .split(',')
-                .map(|p| -> (u32, u32) {
-                    p.split('-')
-                        .map(|n| str::parse::<u32>(n).ok().unwrap())
-                        .next_tuple()
-                        .unwrap()
-                })
-                .next_tuple()
-                .unwrap();
-
-            contains(first, second) || contains(second, first)
-        })
+    pairs()
+        .filter(|(first, second)| contains(first, second) || contains(second, first))
         .count()
         .try_into()
         .unwrap()
 }
 
 pub fn part2() -> u32 {
-    read_lines("inputs/day04.txt")
-        .filter(|line| {
-            let (first, second) = line
-                .split(',')
-                .map(|p| -> (u32, u32) {
-                    p.split('-')
-                        .map(|n| str::parse::<u32>(n).ok().unwrap())
-                        .next_tuple()
-                        .unwrap()
-                })
-                .next_tuple()
-                .unwrap();
-
-            first.0 <= second.1 && first.1 >= second.0
-        })
+    pairs()
+        .filter(|(first, second)| first.0 <= second.1 && first.1 >= second.0)
         .count()
         .try_into()
         .unwrap()
