b027972eb56a62eb5a04781a0688df276c51971f
Michael Vo <zax@zaxu.xyz>
2022-12-07 00:24:21 +1100

    refactor(day06): use as_bytes

--- src/day06.rs
+++ src/day06.rs
@@ -13,12 +13,11 @@ fn start_of_packet_end(len: usize) -> usize {
     read_lines("inputs/day06.txt")
         .next()
         .unwrap()
-        .chars()
-        .collect::<Vec<char>>()
+        .as_bytes()
         .windows(len)
         .enumerate()
         .into_iter()
-        .find(|(_, w)| HashSet::<&char>::from_iter(w.into_iter()).len() == len)
+        .find(|(_, w)| HashSet::<_>::from_iter(w.into_iter()).len() == len)
         .unwrap()
         .0
         + len
