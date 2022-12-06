61ce6996e5f371c2abda9f55dc61ca60875359f1
Michael Vo <zax@zaxu.xyz>
2022-12-07 00:21:24 +1100

    feat(day06): part 2

--- src/day06.rs
+++ src/day06.rs
@@ -2,20 +2,24 @@ use crate::util::read_lines;
 use std::collections::HashSet;
 
 pub fn part1() -> usize {
+    start_of_packet_end(4)
+}
+
+pub fn part2() -> usize {
+    start_of_packet_end(14)
+}
+
+fn start_of_packet_end(len: usize) -> usize {
     read_lines("inputs/day06.txt")
         .next()
         .unwrap()
         .chars()
         .collect::<Vec<char>>()
-        .windows(4)
+        .windows(len)
         .enumerate()
         .into_iter()
-        .find(|(_, w)| HashSet::<&char>::from_iter(w.into_iter()).len() == 4)
+        .find(|(_, w)| HashSet::<&char>::from_iter(w.into_iter()).len() == len)
         .unwrap()
         .0
-        + 4
-}
-
-pub fn part2() -> u32 {
-    0
+        + len
 }
